63f0ac84626882b51dd4e3e4a6dfbb08b74ca601
Stayer2109 <nthanhphong2109@gmail.com>
2025-02-25 20:40:12 +0700

    feat: almost finish sidebar

--- src/AtomicComponents/organisms/Header/Header.jsx
+++ src/AtomicComponents/organisms/Header/Header.jsx
@@ -1,7 +1,7 @@
 import Logo from "../../../assets/icon/dasrs-logo.png";
 import { SidebarIcon } from "../../../assets/icon-svg";
 import { Link } from "react-router-dom";
-import Connector from "../../../assets/icon/dasrs-logo.png";
+import Connector from "../../../assets/icon/connector.jpg";
 
 export default function Header() {
   return (
@@ -15,28 +15,52 @@ export default function Header() {
       </div>
 
       {/* Right Side: Lumpy Nav + Circle Icon */}
-      <div className="flex items-center group gap-10">
+      <div className="flex items-center group">
         {/* First Item */}
-        <ul className="hidden group-hover:flex">
-          <li
-            className={`relative bg-gray-700 text-white px-standard-x py-standard-y rounded-3xl
-        after:content-[''] after:absolute 
-        after:w-16 after:h-8
-        after:top-1/2 after:-translate-y-1/2
-        after:z-2
-        after:bg-[url("${Connector}")] 
-        after:bg-cover after:bg-center after:bg-no-repeat after:bg-transparent`}
-          >
-            <Link>Kamehameha</Link>
-          </li>
-        </ul>
+        <div className="item-container hidden group-hover:flex group-hover:gap-2">
+          <ul>
+            <li
+              className={`relative bg-gray-main text-white px-standard-x py-standard-y rounded-3xl z-1 after:content-[''] right-[8px]
+              after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2 after:z-[-1]
+              after:right-[-32px]
+              after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
+              after:bg-no-repeat after:bg-transparent`}
+            >
+              <Link>Kamehameha</Link>
+            </li>
+          </ul>
 
-        <div className={`bg-[url(${Connector})] w-10 h-8`} />
+          {/* Second Item */}
+          <ul>
+            <li
+              className={`relative bg-gray-main text-white px-standard-x py-standard-y rounded-3xl z-1 after:content-[''] right-[8px]
+              after:absolute after:w-16 after:h-8 after:top-1/2 after:-translate-y-1/2 after:z-[-1]
+              after:right-[-36px]
+              after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
+              after:bg-no-repeat after:bg-transparent`}
+            >
+              <Link>Rasengan</Link>
+            </li>
+          </ul>
+
+          {/* Third Item */}
+          <ul>
+            <li
+              className={`relative bg-gray-main text-white px-standard-x py-standard-y rounded-3xl z-1 after:content-[''] right-[8px]
+              after:absolute after:w-16 after:h-8 after:top-1/2 after:-translate-y-1/2 after:z-[-1]
+              after:right-[-36px]
+              after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
+              after:bg-no-repeat after:bg-transparent`}
+            >
+              <Link>Boom</Link>
+            </li>
+          </ul>
+        </div>
 
         {/* Circle button with the icon. Offset to overlap the last pill */}
         <button
           type="button"
-          className="bg-gray-700 w-14 h-14 rounded-full flex items-center justify-center"
+          className="bg-gray-main w-14 h-14 rounded-full flex items-center justify-center z-1"
         >
           <SidebarIcon className="w-6 h-6" color={"white"} />
         </button>
