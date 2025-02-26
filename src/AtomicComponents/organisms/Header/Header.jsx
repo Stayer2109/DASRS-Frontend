27ff7aa538283f54a48a1d13d714d751f4a6d968
Stayer2109 <nthanhphong2109@gmail.com>
2025-02-26 12:14:11 +0700

    feat: finish header

--- src/AtomicComponents/organisms/Header/Header.jsx
+++ src/AtomicComponents/organisms/Header/Header.jsx
@@ -18,46 +18,52 @@ export default function Header() {
       <div className="flex items-center group">
         {/* First Item */}
         <div
-          className="item-container flex w-auto translate-x-[100px] opacity-0 translate-y-[2px] gap-2 transition 
+          className="item-container w-auto translate-x-[100px] opacity-0 translate-y-[2px] transition 
           duration-300 ease-in-out group-hover:translate-x-0 group-hover:opacity-100">
-          <ul>
+          <ul className="flex gap-2">
             <li
-              className={`relative bg-gray-main text-white px-standard-x py-standard-y rounded-3xl z-1 after:content-[''] right-[8px]
-              after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2 after:z-[-1]
-              after:right-[-32px]
-              after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
-              after:bg-no-repeat after:bg-transparent`}>
-              <Link className="py-1 px-5 hover:bg-gray-main-hover rounded-3xl trasition ease-in-out duration-150">
-                Kamehameha
-              </Link>
+              className={`relative bg-gray-main text-white rounded-3xl after:content-[''] right-[8px] px-[8px] py-1
+                after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2
+                after:right-[-32px]
+                after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
+                after:bg-no-repeat after:bg-transparent`}
+              >
+							<Link className="relative px-standard-x block leading-8 before:absolute before:top-0 before:left-0 before:z-[-1] before:h-full before:w-0
+								before:bg-gray-main-hover before:transition-all before:duration-500 hover:before:w-full
+								before:opacity-0 hover:before:opacity-100 before:rounded-3xl trasition ease-[cubic-bezier(0.4, 0, 1, 1)] duration-150 z-1"
+							>
+								Kamehameha
+							</Link>
             </li>
-          </ul>
 
-          {/* Second Item */}
-          <ul>
             <li
-              className={`relative bg-gray-main text-white px-standard-x py-standard-y rounded-3xl z-1 after:content-[''] right-[8px]
-              after:absolute after:w-16 after:h-8 after:top-1/2 after:-translate-y-1/2 after:z-[-1]
-              after:right-[-36px]
-              after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
-              after:bg-no-repeat after:bg-transparent`}>
-              <Link className="py-1 px-5 hover:bg-gray-main-hover rounded-3xl trasition ease-in-out duration-150">
-                Rasengan
-              </Link>
+              className={`relative bg-gray-main text-white rounded-3xl after:content-[''] right-[8px] px-[8px] py-1
+                after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2
+                after:right-[-32px]
+                after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
+                after:bg-no-repeat after:bg-transparent`}
+              >
+							<Link className="relative px-standard-x block leading-8 before:absolute before:top-0 before:left-0 before:z-[-1] before:h-full before:w-0
+								before:bg-gray-main-hover before:transition-all before:duration-500 hover:before:w-full
+								before:opacity-0 hover:before:opacity-100 before:rounded-3xl trasition ease-[cubic-bezier(0.4, 0, 1, 1)] duration-150 z-1"
+							>
+								Rasengan
+							</Link>
             </li>
-          </ul>
 
-          {/* Third Item */}
-          <ul>
-            <li
-              className={`relative bg-gray-main text-white px-standard-x py-standard-y rounded-3xl z-1 after:content-[''] right-[8px]
-              after:absolute after:w-16 after:h-8 after:top-1/2 after:-translate-y-1/2 after:z-[-1]
-              after:right-[-36px]
-              after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
-              after:bg-no-repeat after:bg-transparent`}>
-              <Link className="py-1 px-5 hover:bg-gray-main-hover rounded-3xl trasition ease-in-out duration-150">
-                Boom
-              </Link>
+						<li
+              className={`relative bg-gray-main text-white rounded-3xl after:content-[''] right-[8px] px-[8px] py-1
+                after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2
+                after:right-[-32px]
+                after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
+                after:bg-no-repeat after:bg-transparent`}
+              >
+							<Link className="relative px-standard-x block leading-8 before:absolute before:top-0 before:left-0 before:z-[-1] before:h-full before:w-0
+								before:bg-gray-main-hover before:transition-all before:duration-500 hover:before:w-full
+								before:opacity-0 hover:before:opacity-100 before:rounded-3xl trasition ease-[cubic-bezier(0.4, 0, 1, 1)] duration-150 z-1"
+							>
+								Boom
+							</Link>
             </li>
           </ul>
         </div>
@@ -65,8 +71,9 @@ export default function Header() {
         {/* Circle button with the icon. Offset to overlap the last pill */}
         <button
           type="button"
-          className="bg-gray-main w-18 h-18 rounded-full flex items-center justify-center z-1">
-          <SidebarIcon className="w-9 h-9" color={"white"} />
+          className="bg-gray-main w-18 h-18 rounded-full flex items-center justify-center z-1 group"
+				>
+          <SidebarIcon className="w-6 h-6 group-hover:rotate-360 group-hover:scale-150 transition ease-[cubic-bezier( 0.68, 0.19, 0.45, 0.82 )] duration-700" color={"white"} />
         </button>
       </div>
     </div>
