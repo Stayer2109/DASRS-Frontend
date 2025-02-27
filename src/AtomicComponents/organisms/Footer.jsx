92e0f928f8ba5838d936e68ac8ac0ebe292e9b9e
Stayer2109 <nthanhphong2109@gmail.com>
2025-02-27 11:38:54 +0700

    feat: update footer - need to fix overflow in x direction of header

--- src/AtomicComponents/organisms/Footer.jsx
+++ src/AtomicComponents/organisms/Footer.jsx
@@ -1,25 +1,130 @@
 /** @format */
-import FPTSponsorLogo from "../../assets/img/fpt-logo.jpg";
+import { Link } from "react-router-dom";
+import FPTSponsorLogo from "../../assets/img/fpt-logo.png";
+import Logo from "../../assets/icon//dasrs-logo.png";
+import RacingThunder from "../../assets/img/racing-thunder.jpg";
 
 const Footer = () => {
-	return (
-		<footer className='h-auto py-standard-y bg-gray-main text-white'>
-			<div
-				id='sponsor-container'
-				className='flex justify-center py-standard-y'
-			>
-				<img
-					src={FPTSponsorLogo}
-					className='w-xl'
-				/>
-			</div>
+  // Footer's links
+  const infoLinks = [
+    {
+      title: "Race Calendar",
+      links: [{ text: "Overview", path: "/overview" }],
+    },
+    {
+      title: "Results",
+      links: [
+        { text: "Race Results", path: "/race-results" },
+        { text: "Standing", path: "/standing" },
+        { text: "Jeddah Race Report S2", path: "/jeddah-s2" },
+        { text: "Doha Race Report S2", path: "/doha-s2" },
+        { text: "Season 1 Results", path: "/season-1-results" },
+      ],
+    },
+    {
+      title: "News & Media",
+      links: [
+        { text: "Latest News & Media", path: "/latest-news" },
+        { text: "All News", path: "/all-news" },
+        { text: "All Media", path: "/all-media" },
+        { text: "Inside E1 Docuseries", path: "/inside-e1-docuseries" },
+      ],
+    },
+    {
+      title: "Team & Pilots",
+      links: [{ text: "Team & Pilots", path: "/team-pilots" }],
+    },
+  ];
 
-			{/* Divider */}
-			<div className='w-full h-[2px] bg-gray-400' />
+  const footerLinks = [
+    "Contact",
+    "FAQs",
+    "How to watch",
+    "Media",
+    "Partners",
+    "Store",
+  ];
 
-			{/* Information about website such as calendar, results, news, ... */}
-		</footer>
-	);
+  const navHoverStyle = "hover:text-lime-300";
+
+  return (
+    <footer className="h-auto py-6 px-standard-x bg-gray-main text-white">
+      {/* Sponsor Container */}
+      <div id="sponsor-container" className="flex justify-center py-standard-y">
+        {/* Provide alt text for better accessibility */}
+        <img
+          src={FPTSponsorLogo}
+          alt="FPT Sponsor Logo"
+          className="w-xl bg-gray-100 rounded-[8px]"
+        />
+      </div>
+
+      {/* Divider */}
+      <div className="w-full h-[0.5px] bg-[#464646]" />
+
+      {/* Information Section */}
+      <div className="information-container py-2">
+        {/* Using a semantic section or nav could help screen readers */}
+        <nav className="grid-container flex items-center justify-between">
+          {/* Information */}
+          <div className="information grid grid-rows-[auto_auto]">
+            <div className="row-start-1 grid grid-cols-2 gap-x-20 gap-y-10">
+              {infoLinks.map(({ title, links }) => (
+                <ul key={title}>
+                  <h1 className="text-3xl text-lime-300 mb-3">{title}</h1>
+                  {links.map(({ text, path }) => (
+                    <li key={text} className={navHoverStyle}>
+                      <Link to={path} className="text-h4">
+                        {text}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ))}
+            </div>
+
+            <div className="row-start-2 mt-8 h-auto">
+              <ul className="flex gap-5">
+                {footerLinks.map((item, index) => (
+                  <li key={index}>
+                    <Link className={`${navHoverStyle} text-h4`}>{item}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          {/* Divine Line*/}
+          <div className="divine-line self-stretch w-[0.5px] bg-[#464646]" />
+
+          <div className="images-show flex flex-col items-start">
+            <img
+              src={RacingThunder}
+              className="h-80 w-auto rounded-[12px]"
+              alt="Logo"
+            />
+            <h2 className="text-h2 w-[500px]">
+              <strong>Dive into exclusive insights </strong>
+              and unparalleled experiences
+            </h2>
+          </div>
+        </nav>
+      </div>
+
+      {/* Divider */}
+      <div className="w-full h-[0.5px] bg-[#464646]" />
+
+      {/* Logo and Socials*/}
+      <div className="flex justify-between pt-6">
+        <div className="">
+          <img src={Logo} className="w-32 bg-blue-500 rounded-full p-2" />
+          <p className="text-[12px] mt-8">
+            Copyright © 2025 DASRS, All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/AtomicComponents/organisms/Header/Header.jsx
+++ src/AtomicComponents/organisms/Header/Header.jsx
@@ -5,88 +5,68 @@ import { SidebarIcon } from "../../../assets/icon-svg";
 import { Link } from "react-router-dom";
 
 export default function Header() {
-	return (
-		<header className='flex justify-between items-center p-4'>
-			{/* Left Side: Logo + Title */}
-			<div className='logo-container flex items-center gap-4'>
-				<img
-					src={Logo}
-					className='h-20 bg-blue-500 rounded-full p-2'
-				/>
-				<h3 className='text-h5 bg-blue-500 text-white px-standard-x py-standard-y rounded-lg'>
-					Driving Assistant Support Racing System
-				</h3>
-			</div>
+  // Nav Items
+  const navItems = ["Kamehameha", "Rasengan", "Boom"];
 
-			{/* Right Side: Lumpy Nav + Circle Icon */}
-			<nav className='flex items-center group'>
-				{/* First Item */}
-				<div
-					className='item-container w-auto translate-x-[100px] opacity-0 translate-y-[2px] transition 
-          duration-300 ease-in-out group-hover:translate-x-0 group-hover:opacity-100'
-				>
-					<ul className='flex gap-2'>
-						<li
-							className={`relative bg-gray-main text-white rounded-3xl after:content-[''] right-[8px] px-[8px] py-1
-                after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2
-                after:right-[-32px]
-                after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
-                after:bg-no-repeat after:bg-transparent`}
-						>
-							<Link
-								className='relative px-standard-x block leading-8 before:absolute before:top-0 before:left-0 before:z-[-1] before:h-full before:w-0
-								before:bg-gray-main-hover before:transition-all before:duration-500 hover:before:w-full
-								before:opacity-0 hover:before:opacity-100 before:rounded-3xl trasition ease-[cubic-bezier(0.4, 0, 1, 1)] duration-150 z-1'
-							>
-								Kamehameha
-							</Link>
-						</li>
+  // Pill Class
+  const pillClass = `relative bg-gray-nav text-white rounded-3xl after:content-[''] right-[8px] px-[8px] py-1
+    after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2
+    after:right-[-32px]
+    after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
+    after:bg-no-repeat after:bg-transparent`;
 
-						<li
-							className={`relative bg-gray-main text-white rounded-3xl after:content-[''] right-[8px] px-[8px] py-1
-                after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2
-                after:right-[-32px]
-                after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
-                after:bg-no-repeat after:bg-transparent`}
-						>
-							<Link
-								className='relative px-standard-x block leading-8 before:absolute before:top-0 before:left-0 before:z-[-1] before:h-full before:w-0
-								before:bg-gray-main-hover before:transition-all before:duration-500 hover:before:w-full
-								before:opacity-0 hover:before:opacity-100 before:rounded-3xl trasition ease-[cubic-bezier(0.4, 0, 1, 1)] duration-150 z-1'
-							>
-								Rasengan
-							</Link>
-						</li>
+  // Link Class
+  const linkClass = `relative px-standard-x block leading-8 before:absolute before:top-0 before:left-0 before:z-[-1] 
+    before:h-full before:w-0 before:bg-gray-main-hover before:transition-all before:duration-500 
+    hover:before:w-full before:opacity-0 hover:before:opacity-100 before:rounded-3xl 
+    trasition ease-[cubic-bezier(0.4, 0, 1, 1)] duration-150 z-1`;
 
-						<li
-							className={`relative bg-gray-main text-white rounded-3xl after:content-[''] right-[8px] px-[8px] py-1
-                after:absolute after:w-14 after:h-8 after:top-1/2 after:-translate-y-1/2
-                after:right-[-32px]
-                after:bg-[url(/src/assets/icon/connector.png)] after:bg-cover after:bg-center 
-                after:bg-no-repeat after:bg-transparent`}
-						>
-							<Link
-								className='relative px-standard-x block leading-8 before:absolute before:top-0 before:left-0 before:z-[-1] before:h-full before:w-0
-								before:bg-gray-main-hover before:transition-all before:duration-500 hover:before:w-full
-								before:opacity-0 hover:before:opacity-100 before:rounded-3xl trasition ease-[cubic-bezier(0.4, 0, 1, 1)] duration-150 z-1'
-							>
-								Boom
-							</Link>
-						</li>
-					</ul>
-				</div>
+  return (
+    <header className="flex justify-between items-center p-4">
+      {/* Left Side: Logo + Title */}
+      <div className="logo-container flex items-center gap-4">
+        {/* Alt text for accessibility */}
+        <img
+          src={Logo}
+          alt="DASRS Logo"
+          className="h-20 bg-blue-500 rounded-full p-2"
+        />
+        <h3 className="text-h5 bg-blue-500 text-white px-standard-x py-standard-y rounded-lg">
+          Driving Assistant Support Racing System
+        </h3>
+      </div>
 
-				{/* Circle button with the icon. Offset to overlap the last pill */}
-				<button
-					type='button'
-					className='bg-gray-main w-18 h-18 rounded-full flex items-center justify-center z-1 group'
-				>
-					<SidebarIcon
-						className='w-6 h-6 group-hover:rotate-360 group-hover:scale-150 transition ease-[cubic-bezier( 0.68, 0.19, 0.45, 0.82 )] duration-700'
-						color={"white"}
-					/>
-				</button>
-			</nav>
-		</header>
-	);
+      {/* Right Side: Lumpy Nav + Circle Icon */}
+      <nav className="flex items-center group">
+        {/* Sliding Nav Items */}
+        <div
+          className="item-container w-auto translate-x-[100px] opacity-0 translate-y-[2px] 
+             transition duration-300 ease-in-out group-hover:translate-x-0 
+             group-hover:opacity-100"
+        >
+          <ul className="flex gap-2">
+            {navItems.map((item) => (
+              <li className={pillClass} key={item}>
+                <Link to="#" className={linkClass}>
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Circle button with the icon (overlaps the last pill) */}
+        <button
+          type="button"
+          className="bg-gray-nav w-18 h-18 rounded-full flex items-center justify-center z-1 group"
+        >
+          <SidebarIcon
+            className="w-6 h-6 group-hover:rotate-360 group-hover:scale-150 
+            transition ease-[cubic-bezier(0.68, 0.19, 0.45, 0.82)] duration-700"
+            color="white"
+          />
+        </button>
+      </nav>
+    </header>
+  );
 }
