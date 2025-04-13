fc5eee7327528e2983c9a241771feff20f4349dc
Stayer2109 <nthanhphong2109@gmail.com>
2025-04-13 11:59:24 +0700

    feat: update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 /** @format */
 
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"; // main.jsx
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
@@ -9,12 +11,12 @@ import { AuthProvider } from "./config/provider/AuthProvider";
 import { Toaster } from "./AtomicComponents/atoms/shadcn/sonner";
 
 createRoot(document.getElementById("root")).render(
-	// <StrictMode>
-		<Router>
-			<AuthProvider>
-				<App /> {/* No need for <Routes> here */}
-				<Toaster />
-			</AuthProvider>
-		</Router>
-	// </StrictMode>
+  // <StrictMode>
+  <Router>
+    <AuthProvider>
+      <App /> {/* No need for <Routes> here */}
+      <Toaster />
+    </AuthProvider>
+  </Router>
+  // </StrictMode>
 );
