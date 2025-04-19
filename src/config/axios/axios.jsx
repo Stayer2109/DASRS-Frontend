7203a819025c4360e8846b85b3ba6a41eff58e48
Stayer2109 <nthanhphong2109@gmail.com>
2025-04-19 16:36:28 +0700

    feat: add extend round, extend tournament ...

--- src/config/axios/axios.jsx
+++ src/config/axios/axios.jsx
@@ -4,13 +4,15 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_BE_BASE_URL;
 
 const apiAuth = axios.create({
-	baseURL: baseURL,
+  baseURL: baseURL,
+  timeout: 10000,
 });
 
 const apiClient = axios.create({
-	baseURL: baseURL,
-	headers: { "Content-Type": "application/json" },
-	withCredentials: true,
+  baseURL: baseURL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+  timeout: 10000,
 });
 
 export { apiAuth, apiClient };
