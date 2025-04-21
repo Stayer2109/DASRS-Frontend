ca507178cf9c7cf7f06600b4f0b8499f023b6adf
leehai1107 <leehai1107@gmail.com>
2025-04-21 21:48:50 +0700

    update: fix some UI bugs

--- src/AtomicComponents/atoms/RoundStatusBadge/RoundStatusBadge.jsx
+++ src/AtomicComponents/atoms/RoundStatusBadge/RoundStatusBadge.jsx
@@ -10,6 +10,8 @@ export const RoundStatusBadge = ({ status }) => {
         return "bg-yellow-100 text-yellow-800";
       case "TERMINATED":
         return "bg-red-100 text-red-800";
+      case "ACTIVE":
+        return "bg-blue-100 text-blue-800";
       default:
         return "bg-gray-100 text-gray-800";
     }
