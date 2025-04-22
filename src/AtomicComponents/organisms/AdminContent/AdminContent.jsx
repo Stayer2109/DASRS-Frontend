0b992be616efb2f34b501fe57f8f51488faf5cee
Le Chi Hai <leehai1107@gmail.com>
2025-04-22 20:47:41 +0700

    update: finish admin dashboard (#155)

--- src/AtomicComponents/organisms/AdminContent/AdminContent.jsx
+++ src/AtomicComponents/organisms/AdminContent/AdminContent.jsx
@@ -9,68 +9,11 @@ import { Environment } from "../Environment/Environment";
 import { Car } from "../Car/Car";
 
 export const AdminContent = ({ activeTab }) => {
-  const todayData = {
-    todayTournamentCount: 15,
-    todayTeamCount: 85,
-    todayPlayerCount: 150,
-    todayRoundCount: 150,
-    todayMatchCount: 320,
-  };
-
-  const weeklyData = {
-    weeks: [
-      {
-        week: "week1",
-        tournamentCount: 3,
-        teamCount: 17,
-        playerCount: 30,
-        roundCount: 30,
-        matchCount: 64,
-      },
-      {
-        week: "week2",
-        tournamentCount: 5,
-        teamCount: 20,
-        playerCount: 40,
-        roundCount: 40,
-        matchCount: 80,
-      },
-      {
-        week: "week3",
-        tournamentCount: 7,
-        teamCount: 25,
-        playerCount: 50,
-        roundCount: 50,
-        matchCount: 100,
-      },
-      {
-        week: "week4",
-        tournamentCount: 10,
-        teamCount: 30,
-        playerCount: 60,
-        roundCount: 60,
-        matchCount: 120,
-      },
-      {
-        week: "week5",
-        tournamentCount: 15,
-        teamCount: 50,
-        playerCount: 100,
-        roundCount: 100,
-        matchCount: 200,
-      },
-    ],
-    totalTournamentCount: 15,
-    totalTeamCount: 85,
-    totalPlayerCount: 150,
-    totalRoundCount: 150,
-    totalMatchCount: 320,
-  };
 
   const renderContent = () => {
     switch (activeTab) {
       case "overview":
-        return <Overview todayData={todayData} weeklyData={weeklyData} />;
+        return <Overview />;
       case "users":
         return <UserManagement />;
       case "tournament":
